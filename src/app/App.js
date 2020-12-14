17ff882368e0ac034ad5ee3b7fd3fc5de23b9d07
esogelola <esogelola@gmail.com>
2020-12-14 13:29:14 -0500

    Added Footer

--- src/app/App.js
+++ src/app/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import Header from "./components/header";
+import Footer from "./components/footer";
 import Introduction from "./components/introduction";
+
 import "../assets/css/app.css";
 export default function App() {
   return (
     <>
       <Header />
       <Introduction />
-      <Introduction />
-      <Introduction />
+      <Footer />
     </>
   );
 }
---
+++ src/app/components/footer.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+export default function footer() {
+  //TODO: Add Footer Icons
+  return (
+    <>
+      <Container className="">
+        <Row>
+          <Col lg="6">
+            <p className="mb-0">
+              Copyrights © {new Date().getFullYear()}. Emmanuel Sogelola
+            </p>
+          </Col>
+          <Col lg="6">
+            <Container className="text-lg-right mt-5 mt-lg-0"></Container>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+}
