4ae673970df39f3d6fe80632992073d2e5840dd3
esogelola <esogelola@gmail.com>
2020-12-15 23:52:47 -0500

    Added Footer Icons, Closes #1

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -3,10 +3,12 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
+import { ImLinkedin2, ImTwitter, ImGithub } from "react-icons/im";
+
 export default function footer() {
   return (
     <>
-      <Container className="">
+      <Container className="mb-2">
         <Row>
           <Col lg="6">
             <p className="mb-0">
@@ -14,7 +16,39 @@ export default function footer() {
             </p>
           </Col>
           <Col lg="6">
-            <Container className="text-lg-right mt-5 mt-lg-0"></Container>
+            <Container className="text-lg-right mt-5 mt-lg-0">
+              <div>
+                <ul className="list-inline mb-0">
+                  <li className="list-inline-item">
+                    <a
+                      href="https://www.linkedin.com/in/emmanuelsogelola/"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <ImLinkedin2 className="mr-3" />
+                    </a>
+                  </li>
+                  <li className="list-inline-item">
+                    <a
+                      href="https://twitter.com/_esog"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <ImTwitter className="mr-3" />
+                    </a>
+                  </li>
+                  <li className="list-inline-item">
+                    <a
+                      href="https://github.com/esogelola/"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <ImGithub />
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </Container>
           </Col>
         </Row>
       </Container>
