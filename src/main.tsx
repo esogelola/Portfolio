9e0dfeb92e5f577a442e57a37aada408fc72177b
Emmanuel Sogelola <sogemman@Emmanuels-MacBook-Pro.local>
2023-09-01 20:51:45 -0400

    added hosting

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
 import App from "./App.tsx";
 import "./index.css";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCGkH6HMwgkGMr1PitQzRCc3lMWu-5s3Eg",
+  authDomain: "esogelola-portfolio.firebaseapp.com",
+  projectId: "esogelola-portfolio",
+  storageBucket: "esogelola-portfolio.appspot.com",
+  messagingSenderId: "69334616443",
+  appId: "1:69334616443:web:8841c4fd61c1782d3c55de",
+  measurementId: "G-17B0J660CG",
+};
+
+const application = initializeApp(firebaseConfig);
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const analytics = getAnalytics(application);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
