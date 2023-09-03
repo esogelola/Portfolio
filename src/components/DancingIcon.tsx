eb5c3c3f2aab0f3941c82586058be1251c814329
Emmanuel Sogelola <sogemman@Emmanuels-MacBook-Pro.local>
2023-09-03 16:34:44 -0400

    Updated v4 code

--- src/components/DancingIcon.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { FaMusic } from "react-icons/fa";
-import "./DancingIcon.css";
-
-const DancingIcon: React.FC = () => {
-  return <FaMusic className="dancing-icon" />;
-};
-
-export default DancingIcon;
